7148e91d754936c009d59f33624fe0863ffd7175
AshuuDixit <72594457+AshuuDixit@users.noreply.github.com>
2021-12-06 22:10:23 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>This is Ashu's new Gatsby site.</p>
+    <p>We will build something great.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
